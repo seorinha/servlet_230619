061aa9f7732b6e7d573d3e810ac796a36f00d7a3
학원 <testtest@gmail.com>
2023-09-04 21:34:14 +0900

    노트북에서 변경을 했다

--- src/main/java/com/test/lesson01/GetMethodEx03.java
+++ src/main/java/com/test/lesson01/GetMethodEx03.java
@@ -13,6 +13,7 @@ public class GetMethodEx03 extends HttpServlet {
 
 	@Override 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//add comment
 		
 		// 한글깨짐 방지 -> plain
 //		response.setCharacterEncoding("utf-8");
